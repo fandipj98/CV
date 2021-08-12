7a9e1782a0a70ea1de8626d590c741f0d3412554
Fandi Pranata Jaya <37364776+fandipj98@users.noreply.github.com>
2021-08-12 20:08:02 +0700

    Add material ui, routing
    - Add material ui Container, Grid
    - Add Router, Switch, Route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
+import { Container, Grid } from "@material-ui/core";
+
+import Profile from "./components/Profile/Profile";
+import Header from "./components/Header/Header";
+import About from "./pages/About/About";
+import Education from "./pages/Education/Education";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      My CV
-    </div>
+    <Container>
+      <Grid container>
+        <Grid items xs={12} sm={12} md={4} lg={3} style={{ backgroundColor: 'yellow' }}>
+          <Profile />
+        </Grid>
+        <Grid items xs>
+          <Header />
+          <Router>
+            <Switch>
+              <Route path="/education">
+                <Education />
+              </Route>
+              <Route path="/">
+                <About />
+              </Route>
+            </Switch>
+          </Router>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
